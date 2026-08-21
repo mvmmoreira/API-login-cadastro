ed1a2fbe081c9cd0a05ce4f0b6c759928cc66097
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-08-21 19:30:11 -0300

    chore: estrutura inicial do projeto via Spring initializr

---
+++ src/main/java/com/mvmmoreira/apilogincadastro/ApilogincadastroApplication.java
@@ -0,0 +1,13 @@
+package com.mvmmoreira.apilogincadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApilogincadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApilogincadastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvmmoreira/apilogincadastro/ApilogincadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvmmoreira.apilogincadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApilogincadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
