9ab063f0a1cb8414eda0d1418c111dcf48a0ceec
Marcos Moreira <vinicius.moreira.nbo@gmail.com>
2026-08-21 23:27:24 -0300

    feat :cria Usuario Repository com métodos de busca por email e cpf

---
+++ src/main/java/com/mvmmoreira/apilogincadastro/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.mvmmoreira.apilogincadastro.repository;
+
+
+import com.mvmmoreira.apilogincadastro.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByCpf(String cpf);
+}
